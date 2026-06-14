d63d476932fc1236f6c223685b031c39cf0800a5
60YKD <ykudo1576@gmail.com>
2026-06-14 20:06:12 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,85 @@
 const CACHE_NAME = "60kai-v6";
 
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+
+
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll([
-"./",
-"./index.html",
-"./manifest.webmanifest"
-]);
-})
-);
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+      .then(cache => {
+
+        return cache.addAll(CACHE_FILES);
+
+      })
+
+  );
+
+  self.skipWaiting();
+
 });
 
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+
+      .then(keys => {
+
+        return Promise.all(
+
+          keys.map(key => {
+
+            if (key !== CACHE_NAME) {
+
+              return caches.delete(key);
+
+            }
+
+          })
+
+        );
+
+      })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(response => {
+
+        if (response) {
+
+          return response;
+
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
 });
